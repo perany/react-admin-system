ec2a387a6ba1c376a0e10b2dde6c2ce5efc71cb7
pengyl <pengyl@kingnet.com>
2019-08-05 13:50:38 +0800

    fix：删除header多余click event

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -56,18 +56,6 @@ class HeaderView extends Component {
 
   handleMenuClick = ({ key }) => {
     const { dispatch } = this.props;
-    if (key === 'userCenter') {
-      router.push('/account/center');
-      return;
-    }
-    if (key === 'triggerError') {
-      router.push('/exception/trigger');
-      return;
-    }
-    if (key === 'userinfo') {
-      router.push('/account/settings/base');
-      return;
-    }
     if (key === 'logout') {
       dispatch({
         type: 'login/logout',

6c2d63a89364e711ca1d81cdc0d0dedf3489e193
pengyl <pengyl@kingnet.com>
2019-08-29 17:14:39 +0800

    fix: 删除多余代码

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ import defaultSettings from "./defaultSettings";
 
 const {pwa, title, primaryColor} = defaultSettings;
 // preview.pro.ant.design only do not use in your production ; preview.pro.ant.design 专用环境变量，请不要在你的项目中使用它。
-const {TEST, NODE_ENV, build_env, MOCK} = process.env;
+const {NODE_ENV, build_env, MOCK} = process.env;
 
 const plugins = [
   [
@@ -74,7 +74,6 @@ export default {
   // 路由配置
   routes: pageRoutes,
   // Theme for antd
-  // https://ant.design/docs/react/customize-theme-cn
   theme: {
     'brand-primary': primaryColor,
     "primary-color": primaryColor,
@@ -95,28 +94,6 @@ export default {
   cssnano: {
     mergeRules: false,
   },
-  // cssLoaderOptions: {
-  //   modules: true,
-  //   getLocalIdent: (context, localIdentName, localName) => {
-  //     if (
-  //       context.resourcePath.includes("node_modules") ||
-  //       context.resourcePath.includes("ant.design.pro.less") ||
-  //       context.resourcePath.includes("global.less")
-  //     ) {
-  //       return localName;
-  //     }
-  //     const match = context.resourcePath.match(/src(.*)/);
-  //     if (match && match[1]) {
-  //       const antdProPath = match[1].replace(".less", "");
-  //       const arr = slash(antdProPath)
-  //         .split("/")
-  //         .map(a => a.replace(/([A-Z])/g, "-$1"))
-  //         .map(a => a.toLowerCase());
-  //       return `antd-pro${arr.join("-")}-${localName}`.replace(/--/g, "-");
-  //     }
-  //     return localName;
-  //   }
-  // },
   manifest: {
     basePath: "/"
   },

bed92d1ea41e839855cc8bd7a59b2c8727348a73
pengyl <pengyl@kingnet.com>
2019-09-06 17:55:34 +0800

    fix: 无用户头像时展示默认icon

--- src/components/GlobalHeader/RightContent.js
+++ src/components/GlobalHeader/RightContent.js
@@ -5,52 +5,57 @@ import styles from './index.less';
 
 export default class GlobalHeaderRight extends PureComponent {
 
-    render() {
-        const {
-            currentUser,
-            onMenuClick,
-          topNavTheme,
-        } = this.props;
-        const menu = (
-            <Menu className={styles.menu} selectedKeys={[]} onClick={onMenuClick}>
-                <Menu.Item key="userCenter">
-                    <Icon type="user"/>
-                    个人中心
-                </Menu.Item>
-                <Menu.Item key="userinfo">
-                    <Icon type="setting"/>
-                    账户设置
-                </Menu.Item>
-                <Menu.Divider/>
-                <Menu.Item key="logout">
-                    <Icon type="logout"/>
-                    退出登录
-                </Menu.Item>
-            </Menu>
-        );
+  render() {
+    const {
+      currentUser,
+      onMenuClick,
+      topNavTheme,
+    } = this.props;
+    const menu = (
+      <Menu className={styles.menu} selectedKeys={[]} onClick={onMenuClick}>
+        <Menu.Item key="userCenter">
+          <Icon type="user"/>
+          个人中心
+        </Menu.Item>
+        <Menu.Item key="userinfo">
+          <Icon type="setting"/>
+          账户设置
+        </Menu.Item>
+        <Menu.Divider/>
+        <Menu.Item key="logout">
+          <Icon type="logout"/>
+          退出登录
+        </Menu.Item>
+      </Menu>
+    );
 
-        let className = styles.right;
-        if (topNavTheme === 'dark') {
-            className = `${styles.right}  ${styles.dark}`;
-        }
-        return (
-            <div className={className}>
-                {currentUser.name ? (
-                    <HeaderDropdown overlay={menu}>
+    let className = styles.right;
+    if (topNavTheme === 'dark') {
+      className = `${styles.right}  ${styles.dark}`;
+    }
+    return (
+      <div className={className}>
+        {currentUser.name ? (
+          <HeaderDropdown overlay={menu}>
             <span className={`${styles.action} ${styles.account}`}>
-              <Avatar
-                  size="small"
-                  className={styles.avatar}
-                  src={currentUser.avatar}
-                  alt="avatar"
-              />
+              {currentUser.avatar ? <Avatar
+                size="small"
+                className={styles.avatar}
+                src={currentUser.avatar}
+                alt="avatar"
+              /> : <Avatar
+                size="small"
+                className={styles.avatar}
+                icon={"user"}
+                alt="avatar"
+              />}
               <span className={styles.name}>{currentUser.name}</span>
             </span>
-                    </HeaderDropdown>
-                ) : (
-                    <Spin size="small" style={{marginLeft: 8, marginRight: 8}}/>
-                )}
-            </div>
-        );
-    }
+          </HeaderDropdown>
+        ) : (
+          <Spin size="small" style={{marginLeft: 8, marginRight: 8}}/>
+        )}
+      </div>
+    );
+  }
 }

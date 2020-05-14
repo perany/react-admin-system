3d139ca695b4c7e555a6399b7c3e883ba683bcf5
perany <perany@163.com>
2020-05-14 18:37:50 +0800

    feat: component PPageHeaderWrapper

---
+++ src/components/PPageHeaderWrapper/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { PageHeader } from 'antd';
+import back from '@/assets/back.svg';
+import { Link, history } from 'umi';
+import styles from './index.less';
+
+const PPageHeaderWrapper = ({
+  children,
+  backRoute,
+  backName,
+}: {
+  children?: any;
+  backRoute?: string;
+  backName?: string;
+}) => {
+  // title text
+  const titleDom = (
+    <div className={styles.title}>
+      <img src={back} style={{ cursor: 'pointer' }} alt="" />
+      <span>{backName || ''}</span>
+    </div>
+  );
+  // title wrap
+  const wrapDom = backRoute ? (
+    <Link to={backRoute} style={{ verticalAlign: 'super', marginRight: 16 }}>
+      {titleDom}
+    </Link>
+  ) : (
+    <a
+      onClick={() => {
+        history.goBack();
+      }}
+      style={{ verticalAlign: 'super', marginRight: 16 }}
+    >
+      {titleDom}
+    </a>
+  );
+
+  return (
+    <PageHeader
+      breadcrumb={{}}
+      title={wrapDom}
+      className={styles.PPageHeaderWrapper}
+      subTitle={null}
+      footer={null}
+    >
+      {children}
+    </PageHeader>
+  );
+};
+
+export default PPageHeaderWrapper;

cb3fdfc5ea0845b8c06342928a6d3111758dee4b
perany <perany@163.com>
2020-05-15 11:09:56 +0800

    fix: proxyConfig mock data

--- config/proxy.config.ts
+++ config/proxy.config.ts
@@ -15,8 +15,8 @@ switch (process.env.build_env ? process.env.build_env : process.env.NODE_ENV) {
   case 'dev':
     // CICD构建：研发环境
     proxyConfig = {
-      postServer: 'https://dev',
-      loginServer: 'http://user.kdcservicedev.kyhub.cn',
+      postServer: 'https://dev.cn',
+      loginServer: 'http://login.cn',
       noticeServer: '',
       projectId: '',
     };
@@ -25,7 +25,7 @@ switch (process.env.build_env ? process.env.build_env : process.env.NODE_ENV) {
     // CICD构建：生产环境
     proxyConfig = {
       postServer: 'https://prod',
-      loginServer: 'http://user.kdcservice.kyhub.cn',
+      loginServer: 'http://login.cn',
       noticeServer: '',
       projectId: '',
     };
@@ -33,8 +33,8 @@ switch (process.env.build_env ? process.env.build_env : process.env.NODE_ENV) {
   case 'development':
     // 本地：研发环境（npm run start）
     proxyConfig = {
-      postServer: 'https://dev',
-      loginServer: 'http://user.kdcservicedev.kyhub.cn',
+      postServer: 'https://dev.cn',
+      loginServer: 'http://login.cn',
       noticeServer: '',
       projectId: '',
     };
@@ -43,7 +43,7 @@ switch (process.env.build_env ? process.env.build_env : process.env.NODE_ENV) {
     // 本地：生产环境 (npm run buildProd)
     proxyConfig = {
       postServer: 'https://prod',
-      loginServer: 'http://user.kdcservicedev.kyhub.cn',
+      loginServer: 'http://login.cn',
       noticeServer: '',
       projectId: '',
     };
@@ -51,8 +51,8 @@ switch (process.env.build_env ? process.env.build_env : process.env.NODE_ENV) {
   default:
     // 默认dev
     proxyConfig = {
-      postServer: 'https://dev',
-      loginServer: 'http://user.kdcservicedev.kyhub.cn',
+      postServer: 'https://dev.cn',
+      loginServer: 'http://login.cn',
       noticeServer: '',
       projectId: '',
     };

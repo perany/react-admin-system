a8786cf7b622bc12be969e34a7375cec0b594c24
perany <perany@163.com>
2020-07-24 11:43:33 +0800

    fix: tslint error

---
+++ src/pages/List/Detail.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from 'antd';
+import PPageHeaderWrapper from '@/components/PPageHeaderWrapper';
+
+const Detail = () => {
+  return (
+    <PPageHeaderWrapper backName="详情页">
+      <div>
+        <Button>返回</Button>
+      </div>
+    </PPageHeaderWrapper>
+  );
+};
+export default Detail;

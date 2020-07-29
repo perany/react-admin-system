3ef4990221e0677882bcbea211479c8ddfefcba1
perany <perany@163.com>
2020-07-29 16:43:19 +0800

    feat：component PTable

--- src/components/PTable/index.tsx
+++ src/components/PTable/index.tsx
@@ -6,11 +6,12 @@ const PTable = (props: any) => {
   const { current, pageSize } = pagination;
   const paginationParams = pagination
     ? {
+        size: 'default',
         showQuickJumper: true,
-        hideOnSinglePage: true,
+        // hideOnSinglePage: true,
         showSizeChanger: true,
-        showTotal: (total: any) =>
-          `共${total}条记录，每页${pageSize}条，共${Math.ceil(total / Number(pageSize))}页`,
+        showTotal: (total: any) => `总计${total}条`,
+        // `共${total}条记录，每页${pageSize}条，共${Math.ceil(total / Number(pageSize))}页`,
         ...pagination,
         current: Number(current),
         pageSize: Number(pageSize),
@@ -18,7 +19,12 @@ const PTable = (props: any) => {
     : false;
   return (
     <Table
-      rowKey={(record, index) => `${index}`}
+      bordered
+      rowKey={(record) =>
+        record && Object.keys(record)?.length > 0
+          ? record?.id ?? JSON.stringify(record)
+          : Math.random()
+      }
       scroll={{ x: true }}
       size="middle"
       {...props}

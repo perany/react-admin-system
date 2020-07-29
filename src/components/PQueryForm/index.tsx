21a4a0928192c408caa5d71051dbddaf614b4c8f
perany <perany@163.com>
2020-07-29 16:43:03 +0800

    feat：component PQueryForm

--- src/components/PQueryForm/index.tsx
+++ src/components/PQueryForm/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { ConfigProvider, Button, DatePicker, Form, Input, Select } from 'antd';
-import styles from './index.less';
+import { ConfigProvider, Button, DatePicker, Form, Input, Select, Radio } from 'antd';
 import { FormProps } from 'antd/es/form';
+
 import { updateURLParams } from '@/utils/utils';
+import PNumberRange from '@/components/PNumberRange';
+import styles from './index.less';
 
 /**
  * example
@@ -28,6 +30,9 @@ import { updateURLParams } from '@/utils/utils';
 
 interface PQueryFormProps extends FormProps {
   items: ItemProps[];
+  updateValues?: (updateCall: any) => void;
+  getFormInstance?: (from: any) => void;
+  searchManual?: boolean;
 }
 
 interface ItemProps {
@@ -35,81 +40,207 @@ interface ItemProps {
   name: string;
   label?: string;
   placeholder?: string;
-  required?: string;
-  render?: () => void;
+  required?: boolean;
+  render?: (item?: any) => void;
   extend?: any;
   data?: any;
 }
 
-const TIME_FORMAT = 'YYYY-MM-DD HH:mm:ss';
+const DATE_FORMAT = 'YYYY-MM-DD';
+const TIME_FORMAT = `${DATE_FORMAT} HH:mm:ss`;
 const { RangePicker } = DatePicker;
 const { Option } = Select;
 
 const PQueryForm = (props: PQueryFormProps) => {
-  const { items, ...rests } = props;
+  const { items, updateValues, getFormInstance, searchManual, ...rests } = props;
   const [form] = Form.useForm();
+
+  // update values
+  if (updateValues) {
+    updateValues((values: any) => {
+      form.setFieldsValue(values);
+    });
+  }
+
+  // update values
+  if (getFormInstance) {
+    getFormInstance(form);
+  }
+
+  // onChange wrap
+  const onChangedCall = (item: any, value: any, callback: any) => {
+    // check if disbaled searchAuto
+    if (searchManual) {
+      return;
+    }
+    // onchange
+    const { extend } = item;
+    if (typeof extend?.onChange === 'function') {
+      const { onChange } = extend;
+      onChange(value);
+    }
+    if (typeof callback === 'function') {
+      callback();
+    }
+  };
+
+  // input press enter
+  const onInputPressEnter = () => {
+    // item count except for buttons
+    const itemsCount = items.filter((val: any) => val?.type !== 'toolbar');
+    if (!searchManual && itemsCount?.length > 1) {
+      form.submit();
+    }
+  };
 
   // type map to dom
   const itemTypeMap = {
+    // 文本输入框
     input: (item: ItemProps) => (
       <Input
         allowClear
         placeholder={`请输入${item.label}`}
         style={{ width: 150 }}
-        onChange={(e) => updateURLParams({ [item.name]: e.target.value })}
-        onPressEnter={() => form.submit()}
+        onPressEnter={() => onInputPressEnter()}
         {...item.extend}
+        onChange={(value: any) =>
+          onChangedCall(item, value, () => {
+            updateURLParams({ [item.name]: value.target.value });
+          })
+        }
       />
     ),
-    select: (item: ItemProps) => (
-      <Select
-        showSearch
-        allowClear
-        placeholder={`请选择${item.label}`}
-        optionFilterProp="children"
-        filterOption={(input: any, option: any) =>
-          option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-        }
-        onChange={() => form.submit()}
-        style={{ minWidth: 150 }}
+    // 下拉搜索框
+    select: (item: ItemProps) => {
+      return (
+        <Select
+          showSearch
+          allowClear
+          placeholder={`请选择${item.label}`}
+          optionFilterProp="children"
+          filterOption={(input: any, option: any) =>
+            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+          }
+          style={{ minWidth: 150 }}
+          {...item.extend}
+          onChange={(value: any) =>
+            onChangedCall(item, value, () => {
+              form.submit();
+            })
+          }
+        >
+          {Array.isArray(item.data) &&
+            [...item.data].map((option: any) => (
+              <Option value={option.value} key={option.value}>
+                {`${option.text}`}
+              </Option>
+            ))}
+        </Select>
+      );
+    },
+    // 日期范围选择框
+    rangePicker: (item: ItemProps) => (
+      <RangePicker
+        format={TIME_FORMAT}
         {...item.extend}
+        onChange={(value: any) => onChangedCall(item, value, () => form.submit())}
+      />
+    ),
+    // 日期选择框
+    datePicker: (item: ItemProps) => (
+      <DatePicker
+        format={DATE_FORMAT}
+        {...item.extend}
+        onChange={(value: any) => onChangedCall(item, value, () => form.submit())}
+      />
+    ),
+    // 按钮组
+    buttonGroup: (item: ItemProps) => (
+      <Radio.Group
+        buttonStyle="solid"
+        {...item.extend}
+        onChange={(value: any) => onChangedCall(item, value, () => form.submit())}
       >
         {Array.isArray(item.data) &&
           [...item.data].map((option: any) => (
-            <Option value={option.value} key={option.value}>
+            <Radio.Button value={option.value} key={option.value}>
               {`${option.text}`}
-            </Option>
+            </Radio.Button>
           ))}
-      </Select>
+      </Radio.Group>
     ),
-    rangePicker: (item: ItemProps) => (
-      <RangePicker format={TIME_FORMAT} onChange={() => form.submit()} {...item.extend} />
-    ),
-    toolbar: ({ name, label, extend }: any) => {
-      const btnProps = { ...extend, key: name };
-      if (name === 'submit') {
+    // 数字范围
+    numberRange: (item: ItemProps) => {
+      const { name, extend = {} } = item;
+      return (
+        <PNumberRange
+          key={name}
+          {...extend}
+          onChange={(value: any) =>
+            onChangedCall(item, value, () => {
+              updateURLParams({ [item.name]: value.target.value });
+            })
+          }
+        />
+      );
+    },
+    // 自定义控件
+    custom: (item: ItemProps) => {
+      if (item?.render) {
+        return item.render(item);
+      }
+      return null;
+    },
+    // 工具按钮
+    toolbar: ({ name, label, extend, render }: any) => {
+      if (render) {
+        return render();
+      }
+      const btnProps = { type: 'default', ...extend, key: name };
+      // toolbar btn type
+      const toolBtnLength = items.filter(
+        (val: any) => val.type === 'toolbar' && val.name !== 'submit',
+      )?.length;
+      if (toolBtnLength < 2) {
         btnProps.type = 'primary';
       }
-      return <Button {...btnProps}>{label || '查询'}</Button>;
+      // submit type
+      if (name === 'submit') {
+        btnProps.type = 'default';
+        btnProps.onClick = (e: any) => {
+          if (extend && typeof extend?.onClick === 'function') {
+            extend.onClick(e);
+          }
+          form.submit();
+        };
+      }
+      return <Button {...btnProps}>{label || '搜索'}</Button>;
     },
   };
 
   // render Form.Item
   const ItemsDom = items.map(
     (item: ItemProps) =>
-      item.type !== 'toolbar' && (
-        <Form.Item label={item.label} name={item.name} key={item.name}>
-          {itemTypeMap[item?.type ?? 'input']?.(item)}
+      (item.type !== 'toolbar' || item.name === 'submit') && (
+        <Form.Item
+          label={item.label}
+          name={item.name}
+          key={item.name}
+          rules={[{ required: item.required, message: '请输入必填字段' }]}
+        >
+          {(itemTypeMap[item?.type ?? 'input'] ?? itemTypeMap.input)?.(item)}
         </Form.Item>
       ),
   );
 
   // render toolbar
-  const toolbarDom = items.filter((item: ItemProps) => item.type === 'toolbar');
+  const toolbarDom = items.filter(
+    (item: ItemProps) => item.type === 'toolbar' && item.name !== 'submit',
+  );
 
   return (
     <ConfigProvider input={{ autoComplete: 'off' }}>
-      <Form form={form} className={styles.PQueryTable} layout="inline" {...rests}>
+      <Form form={form} className={styles.PQueryForm} layout="inline" {...rests}>
         {ItemsDom}
         <Form.Item className={styles.toolBtn}>
           {toolbarDom.map((item) => itemTypeMap.toolbar(item))}

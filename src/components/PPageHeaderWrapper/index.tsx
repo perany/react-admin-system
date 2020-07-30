703100d8c0c67e537f90491216209875227c14da
perany <perany@163.com>
2020-07-30 16:27:52 +0800

    fix: component - PPageHeaderWrapper

--- src/components/PPageHeaderWrapper/index.tsx
+++ src/components/PPageHeaderWrapper/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { PageHeader } from 'antd';
-import { Link, history } from 'umi';
+import { history, Link } from 'umi';
 
 import back from './back.svg';
 import styles from './index.less';
@@ -9,33 +9,57 @@ const PPageHeaderWrapper = ({
   children,
   backRoute,
   backName,
+  props,
+  title,
 }: {
   children?: any;
   backRoute?: string;
   backName?: string;
+  title?: string;
+  props?: any;
 }) => {
   // title text
-  const titleDom = (
-    <div className={styles.title}>
-      <img src={back} style={{ cursor: 'pointer' }} alt="" />
-      <span>{backName || ''}</span>
-    </div>
-  );
-  // title wrap
-  const wrapDom = backRoute ? (
-    <Link to={backRoute} style={{ verticalAlign: 'super', marginRight: 16 }}>
-      {titleDom}
-    </Link>
-  ) : (
-    <a
-      onClick={() => {
-        history.goBack();
-      }}
-      style={{ verticalAlign: 'super', marginRight: 16 }}
-    >
-      {titleDom}
-    </a>
-  );
+  let titleDom: any;
+  if (title) {
+    // no back but only title
+    titleDom = (
+      <div className={styles.title}>
+        <span>{title}</span>
+      </div>
+    );
+  } else {
+    // title with back icon
+    titleDom = (
+      <div className={styles.title}>
+        <img src={back} style={{ cursor: 'pointer' }} alt="" />
+        <span>{backName || ''}</span>
+      </div>
+    );
+  }
+
+  // title Dom
+  let wrapDom: any = titleDom;
+  // back to route
+  if (backRoute) {
+    wrapDom = (
+      <Link to={backRoute} style={{ verticalAlign: 'super', marginRight: 16 }}>
+        {titleDom}
+      </Link>
+    );
+  }
+  // back to last page
+  if (backName) {
+    wrapDom = (
+      <a
+        onClick={() => {
+          history.goBack();
+        }}
+        style={{ verticalAlign: 'super', marginRight: 16 }}
+      >
+        {titleDom}
+      </a>
+    );
+  }
 
   return (
     <PageHeader
@@ -44,6 +68,7 @@ const PPageHeaderWrapper = ({
       className={styles.PPageHeaderWrapper}
       subTitle={null}
       footer={null}
+      {...(props || {})}
     >
       {children}
     </PageHeader>
